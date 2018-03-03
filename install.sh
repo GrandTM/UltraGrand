bdd498d37bb7978e276cb59f21c4a69aa45721b3
GrandTeam <peyma13962017@gmail.com>
2018-03-03 17:52:01 +0330

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-    make_progress() {
-exe=`lua <<-EOF
-    print(tonumber($1)/tonumber($2)*100)
-EOF
-`
-    echo ${exe:0:4}
-}
-    install() {
-            sudo apt update --force-yes &>> /dev/null
             LIB=(
             'libreadline-dev'
             'libconfig-dev' 
@@ -33,15 +24,15 @@ EOF
             )
             local i
             for ((i=0;i<${#LIB[@]};i++)); do
-                apt install ${LIB[$i]} &>> /dev/null
-            done
-    }
-    install_Rocks(){ 
+               sudo apt install ${LIB[$i]} 
+done
+          wget https://valtman.name/files/telegram-cli-1222
+mv telegram-cli-1222 tg
+        chmod +x tg
         wget -O LuaRocks.tar.gz "http://luarocks.org/releases/luarocks-2.2.2.tar.gz"
         tar zxpf LuaRocks.tar.gz
         cd luarocks-2.2.2/
         ./configure; sudo make bootstrap 
-        #####################
         lualibs=(
         'luasec'
         'luasocket'
@@ -61,14 +52,4 @@ EOF
         local i
         for ((i=0;i<${#lualibs[@]};i++)); do
            sudo luarocks install ${lualibs[$i]}
-        done
-    }
-    download_tgcli(){  
-        wget https://valtman.name/files/telegram-cli-1222
-mv telegram-cli-1222 tg
-        chmod +x tg
-    }
-
-install 
-install_Rocks
-download_tgcli
+done
